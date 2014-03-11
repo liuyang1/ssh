f646f049e5386d7cd8bcd28fc063857d9dffff83
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-03-11 17:13:04 +0800

    add write to PNG format

--- cifar/unzip_pic.py
+++ cifar/unzip_pic.py
@@ -1,8 +1,9 @@
+import sys
+import subprocess
 import load
 
 meta, test, train = load.loadData()
 
-print train
 traindata = train["data"]
 filenames = train["filenames"]
 
@@ -11,7 +12,25 @@ assert len(traindata) == len(filenames)
 path = "/tmp/cifar/"
 load.checkDir(path)
 
+convert_flag = False
+if sys.argv[1] == "png":
+    convert_flag = True
+    print "PNG format"
+else:
+    print "PPM format"
+
+
 for i in xrange(len(traindata)):
     data = traindata[i]
     name = path + filenames[i]
+    name += ".ppm"
     load.writePPM(32, 32, data, name)
+    if convert_flag:
+        objname = name[:-4]
+        cmd = ['convert', name, objname]
+        print cmd
+        subprocess.call(cmd)
+        cmd = ['rm', name]
+        subprocess.call(cmd)
+    else:
+        print name
