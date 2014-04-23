4fee90e8435052102d45bf8ba60566588bc313e1
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-04-23 10:59:44 +0800

    update semiSH of North and add APPLY

--- semiSH/NorthoganalPL/apply.py
+++ semiSH/NorthoganalPL/apply.py
@@ -1,7 +1,60 @@
 import numpy as np
+import scipy.spatial
+import sys
+sys.path.append("../..")
+import logger
+log = logger.get()
 
-fo = open("/tmp/w")
-W = np.load(fo)
-fo.close()
-print W
-print W.shape
+
+def hashing(w, x):
+    ret = w * x
+    ret = ret[0, 0]
+    ret = int((1 + np.sign(ret)) / 2)
+    return ret
+
+
+def hashingK(W, x):
+    code = [hashing(w, x) for w in W]
+    return hashing2bit(code)
+
+
+def hashing2bit(x):
+    return scipy.array(x)
+    # return np.packbits(x)[0]
+
+
+def mean2bk(mean, W):
+    return W * mean
+
+
+def hashingKout(W, x, bk):
+    return hashingK(W, x - bk)
+
+if __name__ == "__main__":
+    fo = open("/tmp/ansW")
+    W = np.load(fo)
+    fo.close()
+
+    K = 8
+
+    log.info("W\n%s" % (W))
+    W = W[0:K, :]
+    log.info("W\n%s" % (W))
+    log.info("W shape %s" % (str(W.shape)))
+
+    import semiSH
+    filename = "/home/liuy/obj/gist_batch_1.1000.ans.py.data"
+    X, mean = semiSH.loadX(filename)
+
+    bk = mean2bk(mean, W)
+
+    _, col = X.shape
+    hashingArray = [hashingK(W, X[:, i]) for i in xrange(col)]
+    # print hashingArray
+
+    query = [0, 0, 0, 0, 1, 0, 0, 1]
+    query = scipy.array(query)
+    h = hashingArray[0]
+    r = 2.0 / K
+    rank = [scipy.spatial.distance.hamming(query, h) for h in hashingArray]
+    print [(idx, val) for idx, val in enumerate(rank) if val < r]
--- semiSH/NorthoganalPL/semiSH.py
+++ semiSH/NorthoganalPL/semiSH.py
@@ -7,45 +7,63 @@ from cifar import load
 
 import logger
 log = logger.get()
-if len(sys.argv) == 1:
-    # filename = "/home/liuy/obj/gist_batch_1.1000.ans.py.data"
-    filename = "/home/liuy/obj/gist.ans.py.data"
-else:
-    filename = sys.argv[1]
-fo = open(filename)
-features = pickle.load(fo)
-fo.close()
-log.info("load features")
 
-meta, test, train = load.loadData()
-# labels = load.buildLabelMatrix(train["labels"][0:1000])
-labels = load.buildLabelMatrix(train["labels"])
-log.info("load labels")
+def loadX(filename):
+    fo = open(filename)
+    features = pickle.load(fo)
+    fo.close()
+    log.info("load features")
+    X = np.matrix(features)
+    X = X.T
+    mean = X.mean(1)
+    X -= mean
+    log.info("X zero mean")
+    return X, mean
 
-X = np.matrix(features)
-X = X.T
-Xl = X
-log.info("X ok")
-S = np.matrix(labels)
-log.info("S ok")
-eta = 0.6
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        filename = "/home/liuy/obj/gist_batch_1.1000.ans.py.data"
+        # filename = "/home/liuy/obj/gist.ans.py.data"
+    else:
+        filename = sys.argv[1]
+
+    X, _ = loadX(filename)
+    Xl = X
+    log.info("X ok")
+
+    meta, test, train = load.loadData()
+    labels = load.buildLabelMatrix(train["labels"][0:1000])
+#labels = load.buildLabelMatrix(train["labels"])
+    log.info("load labels")
+
+
+    S = np.matrix(labels)
+    log.info("S ok")
+    eta = 0.6
 
 # M0 = Xl*S*Xl.T
-M0 = Xl * S
-log.info("Xl * S ok")
-M0 = M0 * Xl.T
-log.info("M0=Xl*S*Xl.T ok")
-M1 = eta * X * X.T
-log.info("M1 ok")
-M = M0 + M1
-log.info("M ok")
+    M0 = Xl * S
+    log.info("Xl * S ok")
+    M0 = M0 * Xl.T
+    log.info("M0=Xl*S*Xl.T ok")
+    M1 = eta * X * X.T
+    log.info("M1 ok")
+    M = M0 + M1
+    log.info("M ok")
 
-log.info("M size %s" % (M.shape))
-V, W = np.linalg.eig(M)
-log.info("W \n%s" % (W))
-log.info("V \n%s" % (V))
+    log.info("M size %s" % (str(M.shape)))
+    V, W = np.linalg.eig(M)
+    log.info("W\n%s" % (W))
+    log.info("V\n%s" % (V))
 
-fw = open("/tmp/ansW", "wb")
-W.dump(fw)
-fw.close()
-log.info("end and quit")
+# ft = open("eig", "w")
+# for i in V:
+#     print i
+#     ft.write("%s\r\n" % (i))
+# ft.close()
+# log.info("write V txt file ok")
+
+    fw = open("/tmp/ansW", "wb")
+    W.dump(fw)
+    fw.close()
+    log.info("end and quit")
